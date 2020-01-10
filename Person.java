11289573cb364943489424f1b54ac9f70c0a3fa6
Pattarawit Polpinit <polpinit@kku.ac.th>
2020-01-11 06:52:30 +0700

    Friday Jan 10

---
+++ Person.java
@@ -0,0 +1,9 @@
+abstract class Person {
+    protected String nationality;
+
+    public abstract String getNationality();
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    } 
+}
