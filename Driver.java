df641768c3e6195e544aa1c9833239b8c4117258
Pattarawit Polpinit <polpinit@kku.ac.th>
2020-01-19 19:31:15 +0700

    Friday Jan 17 class

--- Driver.java
+++ Driver.java
@@ -7,26 +7,5 @@ public class Driver {
         A a = new A();
         a.doSomthing("test");
         a.doSomthing(123);
-
-
-
-        /**
- * Driver
- */
-public class Driver {
-
-    public static void main(String[] args) {
-        A a;
-        a = new B();
-        a.hello();
-        a = new C();
-        a.hello();
-
-        E e = new E();
-        e.methodA();
-        e.methodB(123);
-
-    }
-}
     }
 }
--- HelloWorldGUI.java
+++ HelloWorldGUI.java
@@ -1,7 +1,38 @@
-import javax.swing.JOptionPane;
+import javax.swing.*;
+import java.awt.*;
 
 public class HelloWorldGUI {
    public static void main(String[] args) {
-      JOptionPane.showMessageDialog( null, "Hello World!" );
+
+
+      JFrame window = new JFrame("Test Window 1");
+
+      JButton button1 = new JButton("1");
+      JButton button2 = new JButton("Button 2");
+      JButton button3 = new JButton("b3");
+      JButton button4 = new JButton("Button 4");
+      JButton button5 = new JButton("five");
+      JButton button6 = new JButton("SIX");
+
+      JPanel panel1 = new JPanel(new FlowLayout());
+
+      // FlowLayout layout = new FlowLayout();
+
+      // panel1.setLayout(layout);
+
+      panel1.add(button1);
+      panel1.add(button3);
+      panel1.add(button2);
+      panel1.add(button4);
+      panel1.add(button5);
+      panel1.add(button6);
+
+      window.add(panel1);
+
+      window.setSize(800,100);
+      window.setLocation(500,300);
+      window.setVisible(true);
+
+
    }
 }
