88d026ebc5c3fa86bdc899b2eedb73232cbd0bef
Pattarawit Polpinit <polpinit@kku.ac.th>
2020-03-28 07:18:13 +0700

    Add array and Exception examples

---
+++ Array.java
@@ -0,0 +1,10 @@
+/**
+ * Array
+ */
+public class Array {
+
+    public static void main(String[] args) {
+        int[] number = new int[];
+        
+    }
+}
---
+++ ExceptionExample.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * ExceptionExample
+ */
+public class ExceptionExample {
+
+    public static void main(String[] args) {
+            String input= args[0];
+            int x = Integer.parseInt(input);
+            int result = 2/x;
+    } 
+}
