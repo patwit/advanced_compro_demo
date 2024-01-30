030be55eb8542f86ec9291ae33e9982e74dec863
Pattarawit Polpinit <polpinit@kku.ac.th>
2024-01-30 17:20:04 +0700

    update Jan 30

--- 2023/MyWindow9.java
+++ 2023/MyWindow9.java
@@ -1,6 +1,14 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class MyWindow9 extends MyWindow{
+
+    ReadImage imgPanel;
+    JPanel buttonPanel;
+    
     public MyWindow9(String title) {
         super(title);
         //TODO Auto-generated constructor stub
@@ -22,6 +30,14 @@ public class MyWindow9 extends MyWindow{
 
     @Override
     public void addComponents(){
-        
+        super.addComponents();
+        imgPanel = new ReadImage();
+        imgPanel.setPreferredSize(new Dimension(600, 800));
+        mainPanel.setLayout(new BorderLayout());
+        buttonPanel = new JPanel();
+        buttonPanel.add(cancelButton);
+        buttonPanel.add(okButton);
+        mainPanel.add(imgPanel, BorderLayout.NORTH);
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
     }
 }
